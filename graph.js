09e5395ebc2cac16f10562d9e585882ee40982cc
Depak Borhara <depakborhara@gmail.com>
2020-04-20 17:39:27 -0400

    Live changes, legend, and Tweens added

--- graph.js
+++ graph.js
@@ -22,9 +22,44 @@ const arcPath = d3
   .outerRadius(radius)
   .innerRadius(radius / 2);
 
+const color = d3.scaleOrdinal(d3.schemeTableau10);
+
+const legendGroup = svg
+  .append('g')
+  .attr('transform', `translate(${width + 40}, 10)`);
+
+const legend = d3.legendColor().shape('rect').shapePadding(5).scale(color);
+
 //Update Function
 const update = (data) => {
-  const paths = graph.selectAll('path').data(pie(data))
+  //update color scale
+  color.domain(data.map((d) => d.name));
+
+  legendGroup.call(legend).selectAll('text').attr('fill', 'white');
+
+  //join pie to path elements
+  const paths = graph.selectAll('path').data(pie(data));
+
+  //Exit selection
+  paths.exit().transition().duration(750).attrTween('d', arcTweenExit).remove();
+
+  //handle current DOM updates
+
+  paths.transition().duration(1000).attrTween('d', arcTweenUpdate);
+
+  paths
+    .enter()
+    .append('path')
+    .attr('class', 'arc')
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 3)
+    .attr('fill', (d) => color(d.data.name))
+    .each(function (d) {
+      this._current = d;
+    })
+    .transition()
+    .duration(1000)
+    .attrTween('d', arcTweenEnter);
 };
 
 // data array and firestore call
@@ -52,3 +87,32 @@ db.collection('expenses').onSnapshot((response) => {
 
   update(data);
 });
+
+const arcTweenEnter = (d) => {
+  let i = d3.interpolate(d.endAngle, d.startAngle);
+
+  return function (t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
+
+const arcTweenExit = (d) => {
+  let i = d3.interpolate(d.startAngle, d.endAngle);
+
+  return function (t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
+
+function arcTweenUpdate(d) {
+  //interpolate between current object(this._current) and object change(d)
+  let i = d3.interpolate(this._current, d);
+  //Update the current prop with updated new data(d or i(1))
+  this._current = i(1); // >> d
+
+  return function (t) {
+    return arcPath(i(t));
+  };
+}
